4bf5d3617a8cb8f34c70d23456f3d5d9cad714a4
Mats Henricson <mts.henricson@magine.com>
2016-03-13 19:16:43 +0100

    Users can now get friends

--- src/main/scala/xyztr/User.scala
+++ src/main/scala/xyztr/User.scala
@@ -1,10 +1,19 @@
 package xyztr
 
-import java.security.KeyPair
+import java.security.{PublicKey, KeyPair}
 
 /**
-  * Represents the user of this program. Has a name and private/public keys
+  * Represents the user of this program. Has a name, friends and private/public keys.
   */
-case class User(name: String, keyPair: KeyPair) {
+class User(val name: String, keyPair: KeyPair) {
+  private val friends = new scala.collection.mutable.HashSet[Friend]()
+
   def publicKey() = keyPair.getPublic
+
+  def friendRequest(fr: FriendRequest): FriendResponse = {
+    friends.add(Friend(fr.name, fr.publicKey))
+    FriendResponse(name, keyPair.getPublic)
+  }
+
+  def hasFriend(publicKeyOfPerhapsFriend: PublicKey) = friends.exists(_.publicKey.equals(publicKeyOfPerhapsFriend))
 }
--- src/test/scala/xyztr/UserTest.scala
+++ src/test/scala/xyztr/UserTest.scala
@@ -4,8 +4,18 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class UserTest extends FlatSpec with Matchers {
   "User" should "have a public key" in {
-    val mats = User("Mats Henricson", KeyGen.createPrivatePublicPair())
+    val mats = new User("Mats Henricson", KeyGen.createPrivatePublicPair())
     val publicKey = mats.publicKey()
     publicKey should not be null
+  }
+
+  "User" should "get friends" in {
+    val mats = new User("Mats Henricson", KeyGen.createPrivatePublicPair())
+    val bengt = new User("Bengt Henricson", KeyGen.createPrivatePublicPair())
+
+    val fr = FriendRequest(bengt.name, bengt.publicKey())
+    mats.friendRequest(fr)
+
+    mats.hasFriend(bengt.publicKey()) should be(true)
   }
 }
