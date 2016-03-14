6286c15e78fc9bdf6c39617d90681bcf42be10d8
Mats Henricson <mts.henricson@magine.com>
2016-03-14 22:37:27 +0100

    Added start and stop times, a more robust compare() function that doesn't overflow after 24 days diff, and one more apply() function to Bubble companion

--- src/main/scala/xyztr/Bubble.scala
+++ src/main/scala/xyztr/Bubble.scala
@@ -7,17 +7,33 @@ import org.ipfs.api.Base58
 /**
   * Represents all data in a bubble.
   */
-case class Bubble(name: String, creatorName: String, createdTime: Long, members: Set[BubbleMember]) extends Ordered[Bubble] {
-  def compare(that: Bubble) = (this.createdTime - that.createdTime).toInt
+case class Bubble(name: String, creatorName: String, startTime: Long, stoppedTime: Long, members: Set[BubbleMember]) extends Ordered[Bubble] {
+  def compare(that: Bubble) = {
+    val milliDiff = this.startTime - that.startTime
+    if (milliDiff < 0) -1
+    else if (milliDiff > 0) +1
+    else 0
+  }
+
   def hasMember(friend: Friend) = members.exists(f => f.base58EncodedPublicKey == Base58.encode(friend.publicKey.getEncoded))
 }
 
 object Bubble {
   def apply(name: String, creator: User, friends: Set[Friend]): Bubble =
-    Bubble(name, creator.name, new Date().getTime,
+    Bubble(name,
+      creator.name,
+      new Date().getTime,
+      0,
       friends.map(f => BubbleMember(f.name, Base58.encode(f.publicKey.getEncoded))) +
         BubbleMember(creator.name, Base58.encode(creator.publicKey().getEncoded)))
 
+  def apply(name: String, creator: User, startTime: Long, stopTime: Long, friends: Set[Friend]): Bubble =
+    Bubble(name,
+      creator.name,
+      startTime,
+      stopTime,
+      friends.map(f => BubbleMember(f.name, Base58.encode(f.publicKey.getEncoded))) +
+        BubbleMember(creator.name, Base58.encode(creator.publicKey().getEncoded)))
 }
 
 case class BubbleInvitation(ipfsHash: String, encryptedEncryptionKey: Array[Byte])
