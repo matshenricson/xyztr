92257f3f9780127d21a0f5bb50be27aaa00f66bb
Mats Henricson <mts.henricson@magine.com>
2016-03-14 16:32:01 +0100

    Bubble hash is now SHA256 and not Base58. Doh!

--- src/main/scala/xyztr/Bubble.scala
+++ src/main/scala/xyztr/Bubble.scala
@@ -1,17 +1,23 @@
 package xyztr
 
-import java.security.PublicKey
+import java.security.{MessageDigest, PublicKey}
 
 import org.ipfs.api.Base58
 
 /**
   * Represents all data in a bubble.
   */
-class Bubble(val name: String, creator: User, private val friends: Set[Friend]) {
+class Bubble(val name: String, val creator: User, private val friends: Set[Friend]) {
   val encryptionKey = Crypto.createSymmetricEncryptionKey()   // TODO: Remove the bubble encryption key from the bubble?
   val members = friends.map(f => BubbleMember(f.name, f.publicKey)) + BubbleMember(creator.name, creator.publicKey())
 
-  def hashOfBytes() = Base58.encode(allDataAsBytes())
+  def sha256OfData() = {
+    val md = MessageDigest.getInstance("SHA-256")
+    md.update(allDataAsBytes())
+    val digest = md.digest()
+
+    Base58.encode(digest)
+  }
 
   // TODO: This is just a short term hack, we need to do something else, since we can't serialize/unserialize like this
   def allDataAsBytes(): Array[Byte] =
--- src/test/scala/xyztr/BubbleTest.scala
+++ src/test/scala/xyztr/BubbleTest.scala
@@ -6,8 +6,8 @@ class BubbleTest extends FlatSpec with Matchers {
   "Bubble" should "get a hash of all bytes" in {
     val mats = new User("Mats Henricson", Crypto.createPrivatePublicPair())
     val bubble = new Bubble("Bubble name", mats, Set.empty)
-    val hashOfBytes = bubble.hashOfBytes()
-    hashOfBytes.length should be(258)
+    val sha256 = bubble.sha256OfData()
+    sha256.length should be(44)
   }
 
   "Bubble" should "should have its creator among the members" in {
