6562a2d04281956e0d90d0c1a03f7786b96b1b4f
Mats Henricson <mts.henricson@magine.com>
2016-03-17 10:58:20 +0100

    Added a way to reject a friend request. Added FriendRequest and FriendResponse companion objects with apply() methods. Plus made them serializable.

--- src/main/scala/xyztr/Friend.scala
+++ src/main/scala/xyztr/Friend.scala
@@ -4,7 +4,7 @@ import java.security.PublicKey
 
 /**
   * Represents a friend in XYZTR, i.e. a person you share bubbles with.
-  * The public key is used to encrypt the symmetric key used to encrypt bubbles and other stuff.
+  * The public key is used to encrypt the symmetric key used to encrypt bubbles and possibly the communication between friends..
   */
 case class Friend(name: String, encodedPublicKey: Array[Byte]) {
   def publicKey = Crypto.getPublicKeyFromEncoded(encodedPublicKey)
@@ -15,8 +15,19 @@ object Friend {
 }
 
 // TODO: Do I really need the two classes below ????
-case class FriendRequest(name: String, publicKey: PublicKey) {
+case class FriendRequest(name: String, encodedPublicKey: Array[Byte]) {
+  def publicKey = Crypto.getPublicKeyFromEncoded(encodedPublicKey)
 }
 
-case class FriendResponse(name: String, publicKey: PublicKey) {
+object FriendRequest {
+  def apply(name: String, publicKey: PublicKey) = new FriendRequest(name, publicKey.getEncoded)
+}
+
+case class FriendResponse(name: String, encodedPublicKey: Array[Byte]) {
+  def publicKey = Crypto.getPublicKeyFromEncoded(encodedPublicKey)
+}
+
+object FriendResponse {
+  def apply(name: String, publicKey: PublicKey) = new FriendResponse(name, publicKey.getEncoded)
+  def apply() = new FriendResponse("", Array.empty[Byte])    // This is a way of rejecting the friend request
 }
