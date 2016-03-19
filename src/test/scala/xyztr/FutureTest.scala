ca36565921b531c1679d5d96ebe085744fd57699
Mats Henricson <mts.henricson@magine.com>
2016-03-19 11:22:24 +0100

    Wrote some tests that experiments with the syntax around futures and options

---
+++ src/test/scala/xyztr/FutureTest.scala
@@ -0,0 +1,46 @@
+package xyztr
+
+import com.twitter.util.{Await, Future}
+import org.scalatest.{FlatSpec, Matchers}
+
+class FutureTest extends FlatSpec with Matchers {
+  "Futures of Some" can "be handled in one way or the other" in {
+    val f1 = Future(Some(77))
+    Await.result(f1.onSuccess {
+      option => if (option.isDefined) option.get should be(77)
+    })
+
+    val f2 = Future(Some(88))
+    Await.result(f2.onSuccess {option =>
+      for {
+        i <- option
+      } yield i should be(88)
+    })
+
+    Future(Some(99)) map { option =>
+      for {
+        i <- option
+      } yield i should be(99)
+    }
+  }
+
+  "Futures of None" can "also be handled in one way or the other" in {
+    val f1 = Future(None)
+    Await.result(f1.onSuccess {
+      option => if (option.isDefined) throw new IllegalStateException("Very weird")
+    })
+
+    val f2 = Future(None)
+    Await.result(f2.onSuccess {option =>
+      for {
+        i <- option
+      } yield throw new IllegalStateException("Utterly weird")
+    })
+
+    Future(None) map { option =>
+      for {
+        i <- option
+      } yield throw new IllegalStateException("Extremely weird")
+    }
+  }
+}
--- src/test/scala/xyztr/TierionTest.scala
+++ src/test/scala/xyztr/TierionTest.scala
@@ -1,11 +1,13 @@
 package xyztr
 
-import com.twitter.util.Await
 import org.scalatest.{FlatSpec, Matchers}
 
 class TierionTest extends FlatSpec with Matchers {
   "TierionClient" can "talk to Tierion" in {
-    val optionOfSubscriptionResponseFuture = TierionClient.getSubscription
-    Await.result(optionOfSubscriptionResponseFuture.onSuccess {optionSubscription => if (optionSubscription.isDefined) println("GET success: " + optionSubscription.get.id)})
+    TierionClient.getSubscription map { option =>
+      for {
+        subscription <- option
+      } yield subscription.id should be(200)
+    }
   }
 }
