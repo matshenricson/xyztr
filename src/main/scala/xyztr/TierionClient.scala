f9fa9d92dd2726fbb45dcece85877328c3688419
Mats Henricson <mts.henricson@magine.com>
2016-03-19 14:55:54 +0100

    Got my first REST call working to Tierion

--- src/main/scala/xyztr/TierionClient.scala
+++ src/main/scala/xyztr/TierionClient.scala
@@ -20,26 +20,39 @@ object TierionClient {
 
   private val client = ClientBuilder()
       .name("tierion")
-      .hosts("tierion.com:443")
-      .tls("tierion.com")
+      .hosts("api.tierion.com:443")
+      .tls("api.tierion.com")
       .codec(com.twitter.finagle.http.Http())
       .hostConnectionLimit(1).build()
 
+  def createRequest(method: Method, uri: String) = {
+    val request = http.Request(method, "/")
+    request.uri = uri
+    request.host = "api.tierion.com"
+    request.headerMap.add("X-Username", "mats@henricson.se")
+    request.headerMap.add("X-Api-Key", "gJHu+wHGIrqceQu+qUWETqmtB4k9ER5GwyZdC/lQ9vA=")
+    request.headerMap.add("Content-Type", "application/json")
+
+    request
+  }
+
   def getSubscription: Future[Option[SubscriptionResponse]] = {
-    val request = http.Request(http.Method.Get, "/")
-    request.host = "tierion.com"
+    val request = createRequest(Method.Get, "/v1/datastores/533")
 
     client(request) map { response => response.status match {
-      case Status.Ok => Some(read[SubscriptionResponse]("{\"id\":\"" + response.getStatusCode() + "\"}"))
-        case _ => None
+      case Status.Ok =>
+        println("Datastore data: " + response.encodeString())
+        Some(read[SubscriptionResponse]("{\"id\":\"" + response.getStatusCode() + "\"}"))
+      case _ =>
+        println("Something went wrong: " + response.toString())
+        None
       }
     }
   }
 
-  def updateSubscription(userId: Long, productId: Long): Future[Unit] = {
-    val json = write(SubscriptionRequest(productId))
-    val request = http.Request(http.Method.Put, s"/superscription/superscription/v1/users/$userId/subscription")
-    request.setContentString(json)
+  def createRecord(productId: Long): Future[Unit] = {
+    val request = createRequest(http.Method.Put, "v1/records")
+    request.setContentString(write(SubscriptionRequest(productId)))
 
     client(request) map { response =>
       response.status match {
